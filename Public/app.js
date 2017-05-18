b412f812b0acd92abbaa033a5cb02d26ce967159
Conor Atkins <Catkins@iamdigital.uk>
2017-05-18 16:00:53 +0100

    Basket Update

--- Public/app.js
+++ Public/app.js
@@ -1,11 +1,32 @@
 var button = document.getElementById('add-to-basket');
 button.onclick = function(e) {
 	e.preventDefault();
-	var basket = [ ];
+	
+	var basket = getBasketIfExists();
 	var courseID = button.getAttribute('data-course-id');
 	var course = {id: courseID, qty: 1, cost: 250};
 	basket.push(course);
 	localStorage.setItem('basket', JSON.stringify(basket));
-	
+	updateBasketDisplay();
+}
+function getBasketIfExists() {
+	var basket;
+	var stored = localStorage.getItem('basket');
+	try{
+		basket = JSON.parse(stored);
+	} catch (e) {
+		basket = [ ];
+	}
+	if (basket instanceof Array) {
+		return basket;
+	} else {
+		return [ ];
+	}
 }
 
+function updateBasketDisplay() {
+	var basket = getBasketIfExists();
+	var basketDisplay = document.getElementById('basket-count');
+	basketDisplay.innerText = basket.length;
+}
+updateBasketDisplay();
