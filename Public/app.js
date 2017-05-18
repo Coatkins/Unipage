7f30d012685608f855a8ffd9ca7a23103aed1ecc
Conor Atkins <Catkins@iamdigital.uk>
2017-05-18 16:54:09 +0100

    New Stuff

--- Public/app.js
+++ Public/app.js
@@ -1,14 +1,18 @@
 var button = document.getElementById('add-to-basket');
-button.onclick = function(e) {
-	e.preventDefault();
+if (button) {
+	button.onclick = function(e) {
+		e.preventDefault();
 	
-	var basket = getBasketIfExists();
-	var courseID = button.getAttribute('data-course-id');
-	var course = {id: courseID, qty: 1, cost: 250};
-	basket.push(course);
-	localStorage.setItem('basket', JSON.stringify(basket));
-	updateBasketDisplay();
+		var basket = getBasketIfExists();
+		var courseID = button.getAttribute('data-course-id');
+		var courseName = button.getAttribute('data-course-name');
+		var course = {id: courseID, name: courseName, qty: 1, cost: 250};
+		basket.push(course);
+		localStorage.setItem('basket', JSON.stringify(basket));
+		updateBasketDisplay();
+	}
 }
+	
 function getBasketIfExists() {
 	var basket;
 	var stored = localStorage.getItem('basket');
@@ -27,6 +31,27 @@ function getBasketIfExists() {
 function updateBasketDisplay() {
 	var basket = getBasketIfExists();
 	var basketDisplay = document.getElementById('basket-count');
+	if (basketDisplay) {
 	basketDisplay.innerText = basket.length;
 }
+
+}
 updateBasketDisplay();
+
+function writeCoursesToPage() {
+	var element = document.getElementById('basket-courses');
+	if (!element) {
+console.log("No element");
+		return;
+	}
+console.log("HI");
+	var basket = getBasketIfExists();
+	var template = '<div class="basket-item">{{ name }} ({{ id }}): {{ price }}</div>'
+	var html = " ";
+	basket.forEach(function(course) {
+		course.name = course.name || "unknown";
+		html += Mustache.render(template, course);
+	})
+	element.innerHTML = html;
+}
+writeCoursesToPage();
